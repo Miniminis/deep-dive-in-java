cc4d9c9252ea47b3c67d1fc827eee1e3e4bda664
minhee.son <minhee4735@gmail.com>
2023-02-20 17:26:37 +0900

    init project

---
+++ deep-java/src/main/java/_01_jvm/Flash.java
@@ -0,0 +1,7 @@
+package _01_jvm;
+
+public class Flash {
+
+    private String name;
+
+}
---
+++ deep-java/src/main/java/_01_jvm/Main.java
@@ -0,0 +1,8 @@
+package _01_jvm;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Java");
+    }
+}
